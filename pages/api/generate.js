61338f76b1ca113360db5a84719a7683b23f12ec
bcodesoft <qc_21c932bd29ae483bb1f4b7fa31dc2ebb@git.code.tencent.com>
2023-02-25 19:17:30 +0800

    00

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,9 +28,12 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      //prompt: generatePrompt(animal),
+      prompt: animal,
+      max_tokens: 2048,
+      temperature: 1,
     });
+    console.log(completion.data)
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
